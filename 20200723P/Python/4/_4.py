9ddd40d643deb53d6747fa3601df262d8aa54ad5
hyojeong00 <gywjd9563@naver.com>
2020-07-23 16:53:38 +0900

    first learn

---
+++ 20200723P/Python/4/_4.py
@@ -0,0 +1,8 @@
+a,b,c,d=0,0,0,0
+hap = 0
+a = int(input("1번째 숫자 : "))  #숫자를 입력받아  a에 그 값을 대입
+b = int(input("2번째 숫자 : "))
+c = int(input("3번째 숫자 : "))
+d = int(input("4번째 숫자 : "))
+hap=a+b+c+d
+print("합계 => %d" %hap)  # %d 는 %뒤에 선언한 hap값 대입
